bf6b32dc68d7fb329ba0a53f665aadaffb8dbd7f
soshiharami <soshi0807sss@gmail.com>
2020-07-26 22:44:37 +0900

    add new page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import Landing from '../sections/Landing';
 import About from '../sections/About';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Skill from '../sections/Skills'
 
 const IndexPage = () => (
   <Layout>
     <Header />
     <Landing />
     <About />
+    <Skill />
     <Footer />
   </Layout>
 );
